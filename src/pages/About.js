cd180b5d390a387b592685d81cb8a6f7e7660245
kettykati <ketanagx@gmail.com>
2025-10-29 15:20:07 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ export default function About(){
       <p style={{maxWidth:800,margin:'0 auto',lineHeight:1.6}}>
         StyleCart is a demo online fashion store built with React. This project demonstrates
         component-based UI, React Router for navigation, and responsive design using CSS.
-        It was created by Kirtan Jaiswal as a practical submission.
+        It was created by Ketan Agarwal as a practical submission.
       </p>
     </div>
   );
